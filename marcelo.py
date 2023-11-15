233d2668cbece2faf7fc76e31d949ed3dadf05e1
Gabriel de Oliveira Souza <64101105+gabrielsouza-oss@users.noreply.github.com>
2023-11-15 14:21:03 -0300

    Update marcelo.py

--- marcelo.py
+++ marcelo.py
@@ -87,7 +87,7 @@ def main():
                     thumb = thumb_generator(output_filename, title)
                     print("Selected thumb: %s" % (thumb))
                     if (thumb != None):
-                       upload_video(output_filename_final, thumb, title, description)
+                       upload_video(output_filename, thumb, title, description)
                 
                 # Move all video files to dir/
                 move_files(title)
